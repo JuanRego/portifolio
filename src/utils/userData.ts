38c42578708664ddebbf2b9e5ac100cb0c8a14f2
juan <juanrego.s@outlook.com>
2022-09-05 15:07:13 -0300

    initial

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Juan Rego",
+  githubUser: "JuanRego",
+  linkedinUser: "juanrego",
+  whatsappNumber: "+5521984996467",
+  emailUser: "juanregodasilva@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
